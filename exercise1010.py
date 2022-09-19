bb02634fdd7c0c056386b296228f93943bb47d13
Scott Evans <scottwins@live.co.uk>
2022-09-19 15:44:44 +0100

    Add files via upload

--- exercise1010.py
+++ exercise1010.py
@@ -4,8 +4,8 @@ try:
     with open(filename, encoding='utf-8') as f:
         contents = f.read()
         words = contents.split()
-        print(contents.count('the')) #any words that contain 'the' ie 'then' or 'there'
-        print(contents.count('the ')) #the word 'the' or words ending in 'the'
+        print(contents.lower().count('the')) #any words that contain 'the' ie 'then' or 'there'
+        print(contents.lower().count('the ')) #the word 'the' or words ending in 'the'
 
 except FileNotFoundError:
     print('sorry, file does not exist')
