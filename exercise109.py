dca0494b530ec8278676d550132f1444bbd9cd36
Scott Evans <scottwins@live.co.uk>
2022-09-19 13:36:30 +0100

    Add files via upload

---
+++ exercise109.py
@@ -0,0 +1,12 @@
+filenames = ['dogs.txt', 'birds', 'cats.txt']
+
+for file in filenames:
+    try:
+        with open(file, encoding='utf-8') as f:
+            contents = f.read()
+        print(contents)
+
+    except FileNotFoundError:
+        pass
+
+
