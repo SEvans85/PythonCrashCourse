bab95764a5504ea281c362e9f3659c4437f4c237
Scott Evans <scottwins@live.co.uk>
2022-09-19 14:13:28 +0100

    Add files via upload

---
+++ exercise1010.py
@@ -0,0 +1,18 @@
+filename = 'alice.txt'
+
+try:
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+        words = contents.split()
+        print(contents.count('the')) #any words that contain 'the' ie 'then' or 'there'
+        print(contents.count('the ')) #the word 'the' or words ending in 'the'
+
+except FileNotFoundError:
+    print('sorry, file does not exist')
+
+
+
+
+
+
+
